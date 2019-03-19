dc320b08fd0fbbd0a2edd4147417d6dfda95ec8f
itggot-martin-nordstrom <martin.nordstrom2@elev.ga.ntig.se>
2019-03-19 08:10:05 +0100

    added notification on item pages

--- site/js/main.js
+++ site/js/main.js
@@ -2,3 +2,41 @@ function toggleSidebar(){
     sidebar = document.querySelector(".sidebar")
     sidebar.classList.toggle("sidebar-active")
 }
+
+function notificationSquad(input){
+    holder = document.querySelector(".notifications-holder")
+    // baseNotification = document.querySelector("#base-notification")
+    // clone = baseNotification.cloneNode(true)
+    if(holder.childElementCount != 0){
+        holder.removeChild(holder.childNodes[0])
+    }
+    
+    notification = document.createElement("div")
+    holder.appendChild(notification)
+    
+    notification.classList.add("notification")
+    header = document.createElement("h1")
+    notification.appendChild(header)
+    header.innerHTML = "Gucci coat : blue : M"
+    
+    subtitle = document.createElement("h2")
+    notification.appendChild(subtitle)
+    subtitle.innerHTML = "Item added"
+    
+    icon = document.createElement("i")
+    notification.appendChild(icon)
+    icon.classList.add("material-icons")
+    icon.innerHTML = "arrow_right"
+
+    if(input == top){
+        notification.classList.add("top-notification")
+    }else{
+        notification.classList.add("bottom-notification")
+    }
+
+    console.log(notification)
+
+    setTimeout(function(){
+        notification.remove()
+    }, 3000)
+}
